20c30aa8b0b9a5e63b2051db7ecf7a77714567ac
00612 <00612@DESKTOP-4C6LN8J>
2024-04-29 15:12:55 +0700

    done ver1

--- DapperASPNetCore/DapperASPNetCore/Program.cs
+++ DapperASPNetCore/DapperASPNetCore/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Web;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +15,8 @@ namespace DapperASPNetCore
 	{
 		public static void Main(string[] args)
 		{
+			var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+
 			CreateHostBuilder(args).Build().Run();
 		}
 
@@ -21,6 +25,11 @@ namespace DapperASPNetCore
 				.ConfigureWebHostDefaults(webBuilder =>
 				{
 					webBuilder.UseStartup<Startup>();
-				});
+				}).ConfigureLogging(logging =>
+				{
+					logging.ClearProviders();
+					logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+				})
+				.UseNLog();
 	}
 }
